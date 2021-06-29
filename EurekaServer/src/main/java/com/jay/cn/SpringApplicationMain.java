0e4d1da2b10c451f3705b3af6de6967393240e42
1347025837@qq.com <ljlmy625>
2021-06-29 17:57:11 +0800

    111

---
+++ EurekaServer/src/main/java/com/jay/cn/SpringApplicationMain.java
@@ -0,0 +1,14 @@
+package com.jay.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringApplicationMain {
+   
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationMain.class);
+    }
+}
---
+++ EurekaServer7002/src/main/java/com/jay/cn/SpringApplicationMain7002.java
@@ -0,0 +1,15 @@
+package com.jay.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringApplicationMain7002 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationMain7002.class);
+    }
+
+}
